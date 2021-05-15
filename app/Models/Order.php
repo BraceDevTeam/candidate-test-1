c289dfe361bd62a7fd777db497c558c3c91c15c6
pizzaspaziale <giulita00@gmail.com>
2021-05-15 19:24:23 +0200

    Inizio backend e creazione order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    public function customer()
-    {
-        return $this->belongsTo(Order::class);
-    }
+    use SoftDeletes;
+
+    protected $fillable = ['text', 'description', 'cost'];
+
 }
+
